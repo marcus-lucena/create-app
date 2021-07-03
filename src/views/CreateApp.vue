2486b4b4a957052c1dc1cb8eeca6922e9d42970e
marcus-lucena <marcus.lucena@outlook.com>
2021-07-03 17:51:00 -0300

    Customizado o botão de upload de imagens e o pick color

--- src/views/CreateApp.vue
+++ src/views/CreateApp.vue
@@ -13,7 +13,17 @@
     </div>
     <div class="field-grouping">
       <label>{{ $t("create-app.labels.icons-background") }}</label>
-      <Verte picker="wheel" model="rgb" :value="iconColor" />
+      <div class="container-color-picker">
+        <div class="input-form">Pick a Color</div>
+        <v-swatches v-model="iconColor">
+          <input
+            slot="trigger"
+            :value="iconColor"
+            :style="{ background: iconColor }"
+            readonly
+          />
+        </v-swatches>
+      </div>
       <span></span>
     </div>
     <div class="field-grouping">
@@ -25,13 +35,14 @@
 </template>
 
 <script>
+import VSwatches from "vue-swatches";
+import vSelect from "vue-select";
 import ImageUpload from "@/components/ImageUpload";
 import Title from "@/components/Title";
 import CreateAppMixin from "@/mixins/CreateApp";
-import Verte from "verte";
-import vSelect from "vue-select";
 
 import "vue-select/dist/vue-select.css";
+import "vue-swatches/dist/vue-swatches.css";
 
 export default {
   name: "CreateApp",
@@ -39,10 +50,48 @@ export default {
   components: {
     ImageUpload,
     Title,
-    Verte,
+    VSwatches,
     vSelect,
+  },
+  methods: {
+    colorSelected() {
+      return "background:" + this.iconColor;
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.container-color-picker {
+  width: 100%;
+  height: 40px;
+  display: flex;
+  position: relative;
+  box-sizing: border-box;
+
+  > div {
+    line-height: 40px;
+  }
+
+  > .vue-swatches {
+    position: absolute;
+    right: 0;
+    z-index: 98;
+
+    > div {
+      > input {
+        width: 100px;
+        height: 40px;
+
+        border: 0;
+        border-radius: 0 0.3rem 0.3rem 0;
+
+        font-size: 1.2rem;
+        color: #ffffff;
+        text-align: center;
+        text-transform: uppercase;
+      }
+    }
+  }
+}
+</style>
